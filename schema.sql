d7bf6b4360d241ab6f81fbfbbc9b75c653df7e13
Ashbel <kashbel747@gmail.com>
2025-07-24 10:21:02 +0300

    Add courses table schema

--- schema.sql
+++ schema.sql
@@ -1,7 +1,16 @@
+/* creating students table */
 CREATE TABLE students (
     id INT PRIMARY KEY,
     first_name VARCHAR(20),
     email VARCHAR(40),
     dob DATE,
     program VARCHAR(10)
+);
+
+/* creating courses table */
+CREATE TABLE courses (
+    id INT PRIMARY KEY,
+    course_name VARCHAR(20),
+    course_code VARCHAR(10),
+    instructor VARCHAR(20)
 );
